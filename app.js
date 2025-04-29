5b2b2b13dbcee9e920ec0b987cb26ebfb5a01be0
YanisHlali <yanis.hlali@gmail.com>
2025-04-29 12:06:17 +0200

    Simplify CORS configuration in app.js by using default settings

--- app.js
+++ app.js
@@ -7,15 +7,7 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(express.json());
-
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors());
 
 app.use('/users', proxy(process.env.USERS_SERVICE_URL, {
   proxyReqPathResolver: req => {
