ef31c4735682e1bd009f2dcc259d3425e305a50e
YanisHlali <yanis.hlali@gmail.com>
2025-04-29 11:59:16 +0200

    Refactor CORS configuration in app.js for improved security and flexibility

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use('/users', proxy(process.env.USERS_SERVICE_URL, {
   proxyReqPathResolver: req => {
